8c6c78c38e4c6f94aedebdb7f28f4f37870a54d0
lucasolivier <lucas.o@live.com>
2015-05-15 18:11:11 -0300

    Added two lang to welcome screen and google analyti

--- session.php
+++ session.php
@@ -26,10 +26,20 @@
             <table id="welcome" class="fullscreen" style="display:none;">
                 <tr><td>
                     <div class="popupWindow">
-                        Bem vindo! <br><br>Insira seu nome para ser identificado por outros dispositivos:<br>
-                        <input id="userInput" type="text" maxlength="20" placeholder="Ex.: Lucas-PC, Junior-Cel"><br>
-                        <input id="terms" type="checkbox"> Aceito os <a id="termsBut" href="#">Termos de Uso</a><br>
-                        <div class="popupButton" onclick="setUserAndStart();">OK</div>
+                    <?php
+                        $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+                        if($lang == "pt") {
+                            echo "Bem vindo! <br><br>Insira seu nome para ser identificado por outros dispositivos:<br>
+                            <input id='userInput' type='text' maxlength='20' placeholder='Ex.: Lucas-PC, Junior-Cel'><br>
+                            <input id='terms' type='checkbox'> Aceito os <a id='termsBut' href='#'>Termos de Uso</a><br>
+                            <div class='popupButton' onclick='setUserAndStart();'>OK</div>";
+                        } else {
+                            echo "Welcome! <br><br>Enter your name to be identified by the other devices:<br>
+                            <input id='userInput' type='text' maxlength='20' placeholder='Eg.: Lucas-PC, Junior-Cel'><br>
+                            <input id='terms' type='checkbox'> I agree with the <a id='termsBut' href='#'>Terms of Use</a><br>
+                            <div class='popupButton' onclick='setUserAndStart();'>OK</div>";
+                        }
+                    ?>                      
                     </div>
                 </td></tr>        
             </table>  
@@ -37,7 +47,13 @@
 
         <section id="section">
             <table id="menu">
-                <tr><td onclick="ShowAbout();">Sobre</td></tr>        
+                <?php
+                    if($lang == "pt") {
+                        echo "<tr><td onclick='ShowAbout();'>Sobre</td></tr>";
+                    } else {
+                        echo "<tr><td onclick='ShowAbout();'>About</td></tr>";
+                    }
+                ?>
             </table>
         </section>
         <script src="Wocket.js"></script>
@@ -49,16 +65,22 @@
         <script src="downloadManager.js"></script>
         <script src="misc.js"></script>
         <script src="main.js"></script>
-            <script> 
-                //if(window.location.protocol == "http:")
-                //alert("https://" + window.location.href.substr(7));
-                //window.location.replace("https://" + window.location.href.substr(7));    
-                            
-            var sessionAddr = "<?php echo $_GET["s"]?>",
-                localIp;
+        <script>    
+            <?php
+                if($lang == "pt")
+                    echo "window.ptbr = true; \n";
+            ?>
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60400657-1', 'auto');
+  ga('send', 'pageview');
+            var localIp;
             function getIP(json) { 
                 localIp = json.ip; 
-                begin();   //only starts after gathering the ip
+                begin("<?php echo $_GET["s"]?>");   //only starts after gathering the ip
             }
         </script>
         <script src="https://api.ipify.org?format=jsonp&callback=getIP"></script>
